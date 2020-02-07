c3eaf100acafb8057ad3a7f77565c442206bb17d
Sneha Narendran <sneha.narendran1@thoughtworks.com>
2020-02-07 16:09:39 +0530

    Refactor | Creating a constructor to initialise the welcome message.|

--- src/com/twu/biblioteca/WelcomePage.java
+++ src/com/twu/biblioteca/WelcomePage.java
@@ -1,7 +1,14 @@
 package com.twu.biblioteca;
 
 class WelcomePage {
+    private String welcomeMessage;
+
+    WelcomePage(String welcomeMessage) {
+        this.welcomeMessage = welcomeMessage;
+    }
+
     String getWelcomeMessage() {
-        return "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+        welcomeMessage = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+        return welcomeMessage;
     }
 }
