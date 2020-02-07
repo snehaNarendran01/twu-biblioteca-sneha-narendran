4f60f9fc349ec94e43b9de35e0e1cbb4957b13ad
Sneha Narendran <sneha.narendran1@thoughtworks.com>
2020-02-07 15:55:29 +0530

    Add |Able to view a welcome message.|

---
+++ src/com/twu/biblioteca/WelcomePage.java
@@ -0,0 +1,7 @@
+package com.twu.biblioteca;
+
+class WelcomePage {
+    String getWelcomeMessage() {
+        return "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+    }
+}
