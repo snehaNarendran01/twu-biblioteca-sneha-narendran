2bb4945541d2c25e03720502879e3e074d65150a
Sneha Narendran <sneha.narendran1@thoughtworks.com>
2020-02-09 19:50:42 +0530

    Requirement 1.2:Refactor| Removed inventory from book viewer.|

--- src/main/java/com/twu/biblioteca/BookViewer.java
+++ src/main/java/com/twu/biblioteca/BookViewer.java
@@ -5,6 +5,10 @@ import java.util.ArrayList;
 public class BookViewer {
     private ArrayList<Book> books;
 
+    public BookViewer(ArrayList<Book> books) {
+        this.books = books;
+    }
+
     public void printBookDetails() {
         for (Book book :
                 books) {
--- src/test/java/com.twu.biblioteca/BookViewerTest.java
+++ src/test/java/com.twu.biblioteca/BookViewerTest.java
@@ -1,29 +1,29 @@
-//package com.twu.biblioteca;
-//
-//import org.junit.jupiter.api.Test;
-//
-//import java.io.ByteArrayOutputStream;
-//import java.io.PrintStream;
-//import java.time.Year;
-//import java.util.ArrayList;
-//import java.util.Collections;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.mockito.Mockito.mock;
-//import static org.mockito.Mockito.when;
-//
-//class BookViewerTest {
-//    @Test
-//    void shouldDisplayTheDetailsOfMultipleBooks() {
-//        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-//        System.setOut(new PrintStream(byteArrayOutputStream));
-//        BookList bookInventory = mock(BookList.class);
-//        when(bookInventory.getBooks()).thenReturn(new ArrayList<>(Collections.singletonList(new Book("Famous Five", "Enid Mary Blyton", Year.of(1942)))));
-//        String expected = "Title: Famous Five Author: Enid Mary Blyton Year Of Publish: 1942\n";
-//        BookViewer bookViewer = new BookViewer(bookInventory);
-//
-//        bookViewer.printBookDetails();
-//
-//        assertEquals(expected, byteArrayOutputStream.toString());
-//    }
-//}
+package com.twu.biblioteca;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.time.Year;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BookViewerTest {
+    @Test
+    void shouldDisplayTheDetailsOfMultipleBooks() {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(byteArrayOutputStream));
+        ArrayList<Book> bookList = new ArrayList<>(Arrays.asList(new Book("Famous Five",
+                "Enid Mary Blyton", Year.of(1942)), new Book("Harry Potter", "J.K. Rowling", Year.of(1997))));
+        String expected = "Title: Famous Five Author: Enid Mary Blyton Year Of Publish: 1942\nTitle: " +
+                "Harry Potter Author: J.K. Rowling Year Of Publish: 1997\n";
+
+        BookViewer bookViewer = new BookViewer(bookList);
+
+        bookViewer.printBookDetails();
+
+        assertEquals(expected, byteArrayOutputStream.toString());
+    }
+}
